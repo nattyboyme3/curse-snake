05c48aaa128177b8ff41ae731f41a9af7d5ed59d
Nat BIggs <nbiggs112@cedarville.edu>
2019-10-25 20:47:04 -0400

    working on walls

---
+++ curse_snake.py
@@ -0,0 +1,152 @@
+import copy
+import time
+import random
+
+random.seed(time.time())
+
+NORTH = 0
+EAST = 1
+SOUTH = 2
+WEST = 3
+
+DIRECTION = ['^', '>', 'V', '<']
+
+class Position():
+	def __init__(self, y=0, x=0):
+		self.x = x
+		self.y = y
+
+	def move(self, d,s=1):
+		if d == NORTH:
+			self.y = self.y - s
+		if d == EAST:
+			self.x = self.x + s
+		if d == SOUTH:
+			self.y = self.y + s
+		if d == WEST:
+			self.x = self.x - s
+
+	def __repr__(self):
+		return '(' + str(self.x) + ',' + str(self.y) + ')'
+
+
+class Snake: 
+	def __init__(self, y=10, x=10, h=20):
+		self.l = 5
+		self.h = h
+		self.d = NORTH
+		self.p = [Position(y=y,x=x)]
+		self.a = [Position(y=y+2, x=x+2)]
+		self.b = []
+		for i in range(self.l): 
+			n = copy.deepcopy(self.p[i])
+			n.move(SOUTH)
+			self.p.append(n)
+
+
+	def turn(self, r=True):
+		if r == True:
+			self.d = ( self.d + 1 ) % 4
+		else: 
+			self.d = ( self.d - 1 ) % 4
+
+	def turn_left(self):
+		self.turn(False)
+
+	def turn_right(self):
+		self.turn(True)
+
+	def grow(self, x):
+		for i in range(x):
+			self.p.append(copy.deepcopy(self.p[self.l-1]))
+		self.l = self.l + x
+
+	def move(self):
+		prv = copy.deepcopy(self.p[0])
+		self.p[0].move(self.d)
+		for i in range(1,self.l+1):
+			nxt = self.p[i]
+			self.p[i] = prv
+			prv = nxt
+
+	def dead(self, maxes):
+		h = self.p[0]
+		for i in range(1,self.l):
+			if self.p[i].x == h.x and self.p[i].y == h.y:
+				return True
+		if h.x <= 0 or h.y <= 0:
+			return True
+		if h.x*2 >= maxes[1]-1 or h.y >= maxes[0]-1:
+			return True
+		for i in self.b:
+			if h.x == i.x and h.y == i.y:
+				return True
+		return False
+
+	def apple(self, maxes):
+		spawned = False
+		while not spawned:
+			spawed = True
+			y = random.randint(1,maxes[0]-2)
+			x = random.randint(1,int(maxes[1]/2)-2)
+			for i in self.b:
+				if i.x == x and i.y == y:
+					spawned = False
+		self.a.append(Position(y,x))
+
+	def eat(self, growth):
+		h = self.p[0]
+		for i in self.a:
+			if h.x == i.x and h.y == i.y:
+				self.grow(growth)
+				self.a.remove(i)
+				return True
+
+	def block(self, maxes):
+		for i in range(self.h):
+			y = random.randint(1,maxes[0]-2)
+			x = random.randint(1,int(maxes[1]/2)-2)
+			length = random.randint(3,7)
+			direction = random.randint(0,3)
+			if x == 10 and (y >= 10 and y <=15):
+				continue
+			wall = Position(y,x)
+			self.b.append(wall)
+			for j in range(length):
+				next_wall = copy.deepcopy(wall)
+				next_wall.move(direction)
+				if not (next_wall.y > (maxes[0]-2) or 
+						next_wall.x > (maxes[1]*2)-2 or 
+						next_wall.x < 1 or 
+						next_wall.y < 1):
+					if x == 10 and (y >= 10 and y <=15):
+						continue
+					self.b.append(next_wall)
+					wall = next_wall
+				else:
+					continue
+			if x == 10 and (y >= 10 and y <=15):
+				continue
+
+	def __repr__(self):
+		r = ''
+		m = []
+		for x in range(30):
+			m.append(list())
+			for y in range(30):
+				m [x].append('   ')
+		for s in range (self.l):
+			if s == 0:
+				m[self.p[s].x][self.p[s].y] = ' ' + DIRECTION[self.d] + ' '
+			else:
+				m[self.p[s].x][self.p[s].y] = ' * '
+
+		for x in range(30):
+			for y in range(30):
+				r = r + m[y][x]
+			r = r + '|\n'
+		return r
+
+
+
+	
---
+++ snake.py
@@ -0,0 +1,130 @@
+from curse_snake import *
+import curses
+import time
+from sys import argv
+
+class SnakeDead(RuntimeError):
+	pass
+
+class SnakeGame():
+	def __init__(self,infinite=False, start_speed=0.15, difficulty=20):
+		self.snake = Snake(h=difficulty)
+		self.infinite = infinite
+		# initialize curses
+		self.screen = curses.initscr()
+		curses.noecho()
+		curses.cbreak()
+		self.screen.keypad(True)
+		self.screen.leaveok(0)
+		self.screen.nodelay(True)
+		self.screen.border()
+		self.snake.block(self.screen.getmaxyx())
+		self.ticks = 0
+		self.speed = start_speed
+		self.growth = 1
+		self.min_apples = 1
+		self.points =0
+		self.apple_char = "Q"
+		self.blocking_char = 'X'
+
+	def print_snake(self):
+		if not self.infinite:
+			self.screen.clear()
+		self.screen.border()
+		if len(self.snake.a) < self.min_apples:
+			self.snake.apple(self.screen.getmaxyx())
+		if self.snake.eat(self.growth):
+			curses.beep()
+			self.points = self.points + 10
+		if self.snake.dead(self.screen.getmaxyx()):
+			raise SnakeDead
+		for i in range(self.snake.l):
+			if i == 0: 
+				char = DIRECTION[self.snake.d]
+			else:
+				char = '*'
+			self.screen.addstr(self.snake.p[i].y,(self.snake.p[i].x*2),char)
+		for i in range(len(self.snake.a)):
+			self.screen.addstr(self.snake.a[i].y,(self.snake.a[i].x*2), self.apple_char)
+		for i in range(len(self.snake.b)):
+			self.screen.addstr(self.snake.b[i].y,(self.snake.b[i].x*2), self.blocking_char)
+		self.screen.move(0,0)
+		self.screen.refresh()
+
+	def play(self):
+		try:
+			while True:
+				self.print_snake()
+				time.sleep(self.speed)
+				key = self.screen.getch()
+				if key == 97:
+					self.snake.turn_left()
+				elif key == 100:
+					self.snake.turn_right()
+				else:
+					pass
+				self.snake.move()
+				self.ticks = self.ticks + 1
+				if self.ticks % 177 == 0:
+					self.speed = self.speed - .01
+				if self.ticks % 223 == 0:
+					self.growth = self.growth + 1
+				if self.ticks % 453 == 0:
+					self.min_apples = self.min_apples + 1
+				self.points = self.points + self.min_apples
+		
+		except SnakeDead:
+			message1 = 'OH NOOES!!!!!!'
+			message2 = f"Your snake died with {self.points} points"
+			message3 = 'Press any key to try again'
+			message4 = 'Press CTRL+C to exit'
+			midpoint = int(self.screen.getmaxyx()[1]/2)
+			self.screen.addstr(5,midpoint-int(len(message1)/2),message1)
+			self.screen.addstr(8,midpoint-int(len(message2)/2),message2)
+			self.screen.addstr(14,midpoint-int(len(message4)/2),message4)
+			self.screen.refresh()
+		except KeyboardInterrupt:
+			curses.nocbreak()
+			self.screen.keypad(False)
+			curses.echo()
+			curses.endwin()
+			quit()
+		try:
+			time.sleep(3)
+			self.screen.addstr(11,midpoint-int(len(message3)/2),message3)
+			self.screen.nodelay(False)
+			key = self.screen.getch()
+			if key:
+				return True
+		except KeyboardInterrupt:
+			curses.nocbreak()
+			self.screen.keypad(False)
+			curses.echo()
+			curses.endwin()
+			quit()
+
+if __name__ == '__main__':
+	infinite = False
+	speed=0.15
+	difficulty = 20
+	if len(argv) > 1:
+		arg_index = 1
+		if argv[arg_index] == '-h':
+			print(f'Usage: {argv[0]} [infinite] [<speed>] (in milliseconds per move)')
+			exit()
+		if argv[arg_index] == 'infinite':
+			infinite = True
+			arg_index = arg_index + 1
+		try: 
+			speed = float(argv[arg_index])
+			arg_index = arg_index + 1
+		except ValueError:
+			pass
+		try: 
+			difficulty = int(argv[arg_index])
+		except ValueError:
+			pass
+	while True:
+		game = SnakeGame(infinite=infinite, start_speed=speed, difficulty=difficulty)
+		game.play()
+
